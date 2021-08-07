c067278f60ab7a4aa8857f1b289a9180fc4752ee
Ken Cheung <wasd0109.dev@gmail.com>
2021-08-07 18:03:05 +0900

    Create query for blogposts

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
@@ -7,14 +7,37 @@ import Seo from "../components/seo"
 import BlogCard from "../components/BlogCard"
 import * as styles from "./index.module.css"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <div>
-      <BlogCard />
-      <BlogCard />
-    </div>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const blogPosts = data.allMdx.edges
+  console.log(blogPosts)
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <div>
+        {blogPosts.map(blogPost => {
+          return <BlogCard key={blogPost.id} />
+        })}
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query BlogPosts {
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          id
+          slug
+          frontmatter {
+            title
+            excerpt
+            date
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
