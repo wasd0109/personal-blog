3d2c2c64a1648f127eadf4ec409ba4d2c5b566b8
Ken Cheung <wasd0109.dev@gmail.com>
2021-08-07 14:58:11 +0900

    ProfileCard completed

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,18 +1,22 @@
 import * as React from "react"
 
 import "./destyle.css"
+import "./layout.css"
 
 import "bootstrap/dist/css/bootstrap.min.css"
 
 import Navbar from "./Navbar"
+import ProfileCard from "./ProfileCard"
 
 const Layout = ({ children }) => {
   return (
-    <div className="container">
+    <>
       <Navbar />
-      <div className="side"></div>
-      <div className="content"></div>
-    </div>
+      <div className="main">
+        <ProfileCard />
+        <div className="content"></div>
+      </div>
+    </>
   )
 }
 
