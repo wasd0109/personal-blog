e190770d6dc0bf44d637235671fd75f30ae9f4d7
Ken Cheung <wasd0109.dev@gmail.com>
2021-08-08 22:52:05 +0900

    Pagination translation completed

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,22 +2,23 @@ import React from "react"
 import { Link } from "gatsby"
 import * as styles from "./Pagination.module.css"
 import { Container } from "react-bootstrap"
+import { useTranslation } from "gatsby-plugin-react-i18next"
 
 function Pagination({ numPages, currentPages }) {
+  const { t } = useTranslation()
   const previous = currentPages < 2 ? `/pages/${currentPages - 1}` : "/"
   const next = `/pages/${currentPages + 1}`
   const previousDisabled = currentPages === 1 ? true : false
   const nextDisabled = currentPages === numPages ? true : false
-  if (numPages === 1) return null
-
+  // if (numPages === 1) return null
   return (
     <Container fluid>
       <div className={styles.pagination}>
         <Link className={previousDisabled ? styles.disabled : ""} to={previous}>
-          Previous
+          {t("PreviousPage")}
         </Link>
         <Link className={nextDisabled ? styles.disabled : ""} to={next}>
-          Next
+          {t("NextPage")}
         </Link>
       </div>
     </Container>
